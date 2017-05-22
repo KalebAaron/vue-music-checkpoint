2dac18c956a8de0813fddf70a432474922811841
Kaleb Hundersmarck <kalebaaron@live.com>
2017-05-22 09:26:29 -0600

    showing playlists

--- src/components/My-Tunes.vue
+++ src/components/My-Tunes.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="my-tunes">
     <div class="general">
+    <h1> General Music</h1>
       <div class="container">
         <div class = "row" v-for='song in myTunes.general'>
           <div class = "col-sm-1">
@@ -22,12 +23,84 @@
           </div>
           <div class = "col-sm-2"> 
             <div class="dropdown">
-              <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Add to Playlist
+              <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Options
                 <span class="caret"></span></button>
               <ul class="dropdown-menu">
-                <li><a @click="addTrack(song, 'general')">General</a></li>
-                <li><a @click="addTrack(song, 'dopeNasty')">DopeNasty</a></li>
-                <li><a @click="addTrack(song, 'evenDopeNastier')">Even DopeNastier</a></li>
+                <li><a @click="addTrack(song, 'general')">Promote</a></li>
+                <li><a @click="addTrack(song, 'dopeNasty')">Demote</a></li>
+                <li><a @click="addTrack(song, 'evenDopeNastier')">Remove</a></li>
+              </ul>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+<div class="dope-nasty">
+<h1> Dope Nasty Playlist</h1>
+      <div class="container">
+        <div class = "row" v-for='song in myTunes.dopeNasty'>
+          <div class = "col-sm-1">
+            <img :src="song.artworkUrl60" class = "myPhoto">
+          </div>
+          <div class = "col-sm-4">
+            <audio controls class="controls">
+              <source id="myAudio" :src="song.previewUrl">
+            </audio>
+          </div>
+          <div class = "col-sm-2">
+            <p>{{song.collectionName}}</p>
+          </div>
+          <div class ="col-sm-2">
+            <p>{{song.artistName}}</p>
+          </div>
+          <div class = "col-sm-1">
+              <p>{{song.trackName}}</p>
+          </div>
+          <div class = "col-sm-2"> 
+            <div class="dropdown">
+              <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Options
+                <span class="caret"></span></button>
+              <ul class="dropdown-menu">
+                <li><a @click="addTrack(song, 'general')">Promote</a></li>
+                <li><a @click="addTrack(song, 'dopeNasty')">Demote</a></li>
+                <li><a @click="addTrack(song, 'evenDopeNastier')">Remove from Playlist</a></li>
+              </ul>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="dope-nasty">
+<h1> Even Dope Nastier Playlist</h1>
+      <div class="container">
+        <div class = "row" v-for='song in myTunes.evenDopeNastier'>
+          <div class = "col-sm-1">
+            <img :src="song.artworkUrl60" class = "myPhoto">
+          </div>
+          <div class = "col-sm-4">
+            <audio controls class="controls">
+              <source id="myAudio" :src="song.previewUrl">
+            </audio>
+          </div>
+          <div class = "col-sm-2">
+            <p>{{song.collectionName}}</p>
+          </div>
+          <div class ="col-sm-2">
+            <p>{{song.artistName}}</p>
+          </div>
+          <div class = "col-sm-1">
+              <p>{{song.trackName}}</p>
+          </div>
+          <div class = "col-sm-2"> 
+            <div class="dropdown">
+              <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Options
+                <span class="caret"></span></button>
+              <ul class="dropdown-menu">
+                <li><a @click="addTrack(song, 'general')">Promote</a></li>
+                <li><a @click="addTrack(song, 'dopeNasty')">Demote</a></li>
+                <li><a @click="addTrack(song, 'evenDopeNastier')">Remove from Playlist</a></li>
               </ul>
             </div>
           </div>
